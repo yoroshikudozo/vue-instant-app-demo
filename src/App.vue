e562a41405cd6f33dc5a766cec5461ffb1db9143
kudo <yosuke.kudo910@gmail.com>
2020-02-16 10:12:41 +0900

    Add swiper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Swiper msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Swiper from "./components/Swiper.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Swiper
   }
 };
 </script>
 
 <style lang="scss">
+html {
+  height: 100%;
+  background: black;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background: black;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: black;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
 
+// require styles
+import "swiper/dist/css/swiper.css";
+
+Vue.use(VueAwesomeSwiper, {});
 Vue.config.productionTip = false;
 
 new Vue({
