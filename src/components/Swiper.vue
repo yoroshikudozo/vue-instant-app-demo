fa644ca51c25abebddcd21d0365a2dc4240575b9
kudo <yosuke.kudo910@gmail.com>
2020-02-16 11:14:58 +0900

    Split swiper-pagenation

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -4,48 +4,24 @@
     <swiper-slide data-history="faspa">I'm Slide 2</swiper-slide>
     <swiper-slide data-history="takeout">I'm Slide 3</swiper-slide>
     <swiper-slide data-history="list">I'm Slide 4</swiper-slide>
-    <div class="swiper-pagination" slot="pagination">
-      <ul class="swiper-pagination-list">
-        <li
-          class="swiper-pagination-bullet"
-          :class="{ 'is-active': activeIndex === 0 }"
-          @click="toSlide(0)"
-        >
-          home
-        </li>
-        <li
-          class="swiper-pagination-bullet"
-          :class="{ 'is-active': activeIndex === 1 }"
-          @click="toSlide(1)"
-        >
-          faspa
-        </li>
-        <li
-          class="swiper-pagination-bullet"
-          :class="{ 'is-active': activeIndex === 2 }"
-          @click="toSlide(2)"
-        >
-          takeout
-        </li>
-        <li
-          class="swiper-pagination-bullet"
-          :class="{ 'is-active': activeIndex === 3 }"
-          @click="toSlide(3)"
-        >
-          list
-        </li>
-      </ul>
-    </div>
+    <swiper-nav
+      :activeIndex="activeIndex"
+      @toSlide="toSlide"
+      slot="pagination"
+    />
   </swiper>
 </template>
 
 <script>
 import { swiper, swiperSlide } from "vue-awesome-swiper";
+import SwiperNav from "@/components/SwiperNav";
+
 export default {
   name: "Swiper",
   components: {
     swiper,
-    swiperSlide
+    swiperSlide,
+    SwiperNav
   },
   data() {
     return {
@@ -83,42 +59,5 @@ export default {
   position: relative;
   height: 100%;
   background: white;
-}
-.swiper-slide {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.swiper-pagination {
-  position: absolute;
-  bottom: 0;
-}
-.swiper-pagination-list {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-  border-top: 1px solid #ddd;
-}
-.swiper-pagination-bullet {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 25%;
-  height: 63px;
-  background: none;
-  box-sizing: border-box;
-  border-right: 1px solid #ddd;
-  border-radius: 0;
-  color: #2c3e50;
-  opacity: 1;
-}
-.swiper-pagination-bullet:last-child {
-  border-right: none;
-}
-.swiper-pagination-bullet.is-active {
-  font-weight: bold;
 }
 </style>
---
+++ src/components/SwiperNav.vue
@@ -0,0 +1,108 @@
+<template>
+  <nav class="swiper-pagination">
+    <ul class="swiper-pagination-list">
+      <li class="swiper-pagination-bullet">
+        <button
+          href="#"
+          class="swiper-pagination-bullet-button"
+          :class="{ 'is-active': activeIndex === 0 }"
+          @click.prevent="toSlide(0)"
+        >
+          home
+        </button>
+      </li>
+      <li class="swiper-pagination-bullet">
+        <button
+          href="#"
+          class="swiper-pagination-bullet-button"
+          :class="{ 'is-active': activeIndex === 1 }"
+          @click.prevent="toSlide(1)"
+        >
+          faspa
+        </button>
+      </li>
+      <li class="swiper-pagination-bullet">
+        <button
+          href="#"
+          class="swiper-pagination-bullet-button"
+          :class="{ 'is-active': activeIndex === 2 }"
+          @click.prevent="toSlide(2)"
+        >
+          takeout
+        </button>
+      </li>
+      <li class="swiper-pagination-bullet">
+        <button
+          href="#"
+          class="swiper-pagination-bullet-button"
+          :class="{ 'is-active': activeIndex === 3 }"
+          @click.prevent="toSlide(3)"
+        >
+          list
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SwiperNav",
+  props: {
+    activeIndex: Number
+  },
+  methods: {
+    toSlide(index) {
+      this.$emit("toSlide", index);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.swiper-pagination {
+  position: absolute;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.7);
+}
+.swiper-pagination-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  border-top: 1px solid #ddd;
+}
+.swiper-pagination-bullet {
+  display: flex;
+  width: 25%;
+  height: 64px;
+  background: none;
+  box-sizing: border-box;
+  border-right: 1px solid #ddd;
+  border-radius: 0;
+  color: #2c3e50;
+  opacity: 1;
+}
+.swiper-pagination-bullet:last-child {
+  border-right: none;
+}
+.swiper-pagination-bullet-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &.is-active {
+    font-weight: bold;
+  }
+}
+</style>
